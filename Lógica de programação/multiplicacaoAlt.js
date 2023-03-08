cfcb16a193b70848d7078961cda141b501d96ceb
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2023-03-08 14:32:07 -0300

    Create multiplicacaoAlt.js

---
+++ Lógica de programação/multiplicacaoAlt.js
@@ -0,0 +1,8 @@
+function multiplicacao(num1, num2){
+        var soma = 0;
+        for(i = 0; i<num2; i++){
+            soma += num1;
+        }
+        return soma
+    }
+    console.log(multiplicacao(2,5))
