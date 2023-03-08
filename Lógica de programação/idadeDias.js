efda1bcb522be930a5883f0d9d69a7eac27f52e6
let1ci4b <83764695+let1ci4b@users.noreply.github.com>
2023-03-08 14:27:57 -0300

    Update idadeDias.js

--- Lógica de programação/idadeDias.js
+++ Lógica de programação/idadeDias.js
@@ -1,7 +1,6 @@
-function dias(idade){
-        do{
+do{
         idade = prompt("Quantos anos você tem?");
         } while(idade == null || idade == " " || idade == "" || isNaN(idade))
         dias = idade*365;
         alert(`Sua idade é de ${dias} dias`);
-    }
+    
